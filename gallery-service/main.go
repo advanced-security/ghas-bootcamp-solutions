061f763b3033b6f7a8c0d8260f1a8e58883691a4
Chelsea Boling <cmboling@github.com>
2021-05-18 17:35:52 -0700

    Update main.go

--- gallery-service/main.go
+++ gallery-service/main.go
@@ -644,11 +644,11 @@ func authnMiddleware(next http.Handler) http.Handler {
 			log.Printf("AuthN: Received bearer token %s", tokenString)
 			token, err := jwt.ParseWithClaims(tokenString, &OctoClaims{}, func(token *jwt.Token) (interface{}, error) {
 				// Don't forget to validate the alg is what you expect:
-				//if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				//	return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-				//}
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+				}
 			
-				// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+				hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
 				return []byte(configuration.Secret), nil
 			})
 
